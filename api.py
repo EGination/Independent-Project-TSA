c6faf672542c89368ee8a7601c77d441e5a37d9f
cnb.bU6IELg7gFA <deczbdAzpEeD1AxKztbmwM+cnb.bU6IELg7gFA@noreply.cnb.cool>
2026-05-01 23:52:16 +0800

    data-parsing

--- api.py
+++ api.py
@@ -1,19 +1,25 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(
-    api_key=os.environ.get('DEEPSEEK_API_KEY'),
-    base_url="https://api.deepseek.com")
+class DeepSeekAPI:
+    def __init__(self):
+        self.client = OpenAI(
+            api_key=os.environ.get('DEEPSEEK_API_KEY'),
+            base_url="https://api.deepseek.com")
+    
+    def generate_reason(self) -> str:
+        response = self.client.chat.completions.create(
+            model="deepseek-v4-flash",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant"},
+                {"role": "user", "content": "Hello"},
+            ],
+            stream=False,
+            reasoning_effort="high",
+            extra_body={"thinking": {"type": "enabled"}}
+        )
+        return response.choices[0].message.content
 
-response = client.chat.completions.create(
-    model="deepseek-v4-flash",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False,
-    reasoning_effort="high",
-    extra_body={"thinking": {"type": "enabled"}}
-)
-
-print(response.choices[0].message.content)
+if __name__ == "__main__":
+    api = DeepSeekAPI()
+    print(api.generate_reason())
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import os
 from datasets import load_dataset
 
+from api import DeepSeekAPI
+
 DATA_DIR = "./data"
 TRAIN_DIR = os.path.join(DATA_DIR, "train.csv")
 
+LABEL_MAP = {
+    'id': 'id',
+    'review': '评论',
+    'star': '评级',
+    'Location#Transportation': '交通方便',
+    'Location#Downtown': '位于商圈附近',
+    'Location#Easy_to_find': '是否容易寻找',
+    'Service#Queue': '排队时间',
+    'Service#Hospitality': '服务人员态度',
+    'Service#Parking': '停车方便',
+    'Service#Timely': '点菜/上菜速度',
+    'Price#Level': '价格',
+    'Price#Cost_effective': '性价比',
+    'Price#Discount': '折扣力度',
+    'Ambience#Decoration': '装修',
+    'Ambience#Noise': '嘈杂情况',
+    'Ambience#Space': '就餐空间',
+    'Ambience#Sanitary': '卫生情况',
+    'Food#Portion': '分量',
+    'Food#Taste': '口味',
+    'Food#Appearance': '外观',
+    'Food#Recommend': '食物推荐程度'
+}
+
 def main():
     print("Hello from workspace!")
-    dataset = load_dataset("csv", data_files=TRAIN_DIR)
-    print(dataset)
+    dataset = load_dataset("csv", data_files=TRAIN_DIR, split="train")
+    dataset = dataset.rename_columns(LABEL_MAP)
+    # print(dataset.column_names)
+    print(dataset[1])
 
-
+    # api = DeepSeekAPI()
+    # print(api.generate_reason())
 
 if __name__ == "__main__":
     main()
